a6f810683683fe64f5358c756db2794c13b2baa3
JohnMorales <jmorales@gmail.com>
2013-01-30 22:53:00 -0500

    Initial Commit

---
+++ Hypnosister/HypnosisterAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  HypnosisterAppDelegate.h
+//  Hypnosister
+//
+//  Created by John Morales on 1/30/13.
+//  Copyright (c) 2013 John Morales. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HypnosisterAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Hypnosister/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Hypnosister
+//
+//  Created by John Morales on 1/30/13.
+//  Copyright (c) 2013 John Morales. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "HypnosisterAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([HypnosisterAppDelegate class]));
+    }
+}
